a9ea2d893a283d1107c339bea4e29360e580d64f
Basile Picard <4pipopolam@gmail.com>
2026-05-01 01:40:43 +0300

    Embed implicit usings file.

--- QuickGraphics/Globals.cs
+++ QuickGraphics/Globals.cs
@@ -1,3 +1,11 @@
 global using QuickGraphics;
 global using static QuickGraphics.StaticCanvas;
 global using static QuickGraphics.Colors;
+
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Threading;
+global using System.Threading.Tasks;
