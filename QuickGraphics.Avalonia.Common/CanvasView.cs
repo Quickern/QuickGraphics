109fd5ab69ef451d3298347031742521d86a5895
Basile Picard <4pipopolam@gmail.com>
2026-05-05 22:46:43 +0300

    Mouse in avalonia.

--- QuickGraphics.Avalonia.Common/CanvasView.cs
+++ QuickGraphics.Avalonia.Common/CanvasView.cs
@@ -1,12 +1,18 @@
+using Avalonia.Input;
 using Avalonia.OpenGL;
 using Avalonia.OpenGL.Controls;
+using Avalonia.Rendering;
 using Silk.NET.OpenGL;
 
 namespace QuickGraphics.Avalonia.Common;
 
-public class CanvasView(Canvas canvas) : OpenGlControlBase
+public class CanvasView(Canvas canvas) : OpenGlControlBase, ICustomHitTest
 {
     private readonly Canvas _canvas = canvas;
+
+    private global::Avalonia.Point _mousePosition;
+
+    public bool HitTest(global::Avalonia.Point point) => Bounds.Contains(point);
 
     protected override void OnOpenGlInit(GlInterface gl)
     {
@@ -18,6 +24,8 @@ public class CanvasView(Canvas canvas) : OpenGlControlBase
     protected override void OnOpenGlRender(GlInterface gl, int fb)
     {
         double scale = TopLevel.GetTopLevel(this).RenderScaling;
+
+        _canvas.PrepareMouse(new Point((int)_mousePosition.X, (int)_mousePosition.Y));
 
         _canvas.Prepare(new Canvas.FrameData(new Size((int)Bounds.Width, (int)Bounds.Height), new Size((int)(Bounds.Width * scale), (int)(Bounds.Height * scale))));
 
@@ -42,6 +50,13 @@ public class CanvasView(Canvas canvas) : OpenGlControlBase
         }
 
         return new global::Avalonia.Size(width, height);
+    }
+
+    protected override void OnPointerMoved(PointerEventArgs e)
+    {
+        base.OnPointerMoved(e);
+
+        _mousePosition = e.GetPosition(this);
     }
 
     protected override void OnOpenGlDeinit(GlInterface gl)
--- QuickGraphics.Avalonia.Common/Globals.cs
+++ QuickGraphics.Avalonia.Common/Globals.cs
@@ -1,3 +1,9 @@
 global using Avalonia;
 global using Avalonia.Controls;
 global using Avalonia.Markup.Xaml;
+
+global using QuickGraphics;
+global using QuickGraphics.Mathematics;
+
+global using Point = QuickGraphics.Mathematics.Point;
+global using Size = QuickGraphics.Mathematics.Size;
